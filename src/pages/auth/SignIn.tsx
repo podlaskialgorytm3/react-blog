9eaf445c78e054b39ea4bf2d0b5c1d96ac88d820
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-02 18:22:02 +0100

    feat: Create a sign in from

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,7 +1,9 @@
+import SignInForm from "../../features/auth/components/sign-in";
+
 export const SignIn = () => {
     return (
         <>
-        <h1>Stona do logowania.</h1>
+            <SignInForm />
         </>
     )
 }
