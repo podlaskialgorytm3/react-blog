02a2282d83c3a67f74cfeb056d72fd2081c46bc4
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-02 15:54:35 +0100

    revert: reinstall material-ui to use icon

--- src/shared/components/Menu.tsx
+++ src/shared/components/Menu.tsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router-dom"
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
 
 export const Menu = () => {
     return (
-        <nav className="w-11 h-20 bg-red-500">
-            <NavLink to="/">Strona główna</NavLink>
-            <NavLink to="/signin">Zaloguj się</NavLink>
-            <NavLink to="/signup">Zarejestruj się</NavLink>
+        <nav className="w-full h-[100px] bg-[white] p-4 shadow-md flex justify-center">
+            <div className="flex justify-between items-center h-full w-[80%]">
+                <NavLink to="/"><HomeWorkIcon sx={{color: "black",fontSize: "40px"}} /> micheal-react-blog</NavLink>
+                <div className="w-[200px] flex justify-between">
+                    <NavLink to="/signin" className="w-[100px] h-[60px] text-center leading-[60px]">Sign In</NavLink>
+                    <NavLink to="/signup" className="bg-[#41c48b] w-[100px] h-[60px] text-center leading-[60px] text-white rounded-2xl hover:bg-[#328a63]">Sign Up</NavLink>
+                </div>
+            </div>
         </nav>
     )
 }
