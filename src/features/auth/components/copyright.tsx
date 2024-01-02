5ccff556c23ed7013f6787058e950bf4b8909ea7
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-02 18:09:57 +0100

    feat: create copyright component

---
+++ src/features/auth/components/copyright.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+export function Copyright(props: any) {
+    return (
+      <Typography margin="30px" variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <NavLink to='/' className="text-[#41c48b] font-bold">
+          michael-react-blog
+        </NavLink>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
