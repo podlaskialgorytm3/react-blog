e9bce07f12185c091b94798ae70c8fd7f74a20a1
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-02 15:03:45 +0100

    feat: create simple route with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import { Root } from "./pages/root/Root"
+import { SignIn } from "./pages/auth/SignIn"
+import { SignUp } from "./pages/auth/SignUp"
+
+
+const route = createBrowserRouter([
+  {path: '/', element: <Root />},
+  {path: '/signin', element: <SignIn />},
+  {path: '/signup', element: <SignUp />},
+])
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <RouterProvider router={route}/>
   )
 }
 
