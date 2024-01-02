16168230985fa85ae7ba2f0254b7bc09332b1b5f
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-02 15:19:45 +0100

    fix: Solve problem with installing tailwind

--- src/shared/components/Menu.tsx
+++ src/shared/components/Menu.tsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom"
 
 export const Menu = () => {
     return (
-        <>
+        <nav className="w-11 h-20 bg-red-500">
             <NavLink to="/">Strona główna</NavLink>
             <NavLink to="/signin">Zaloguj się</NavLink>
             <NavLink to="/signup">Zarejestruj się</NavLink>
-        </>
+        </nav>
     )
 }
