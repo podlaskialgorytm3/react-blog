f527bc921d09b811ea5001d1dddcffa3435e75ee
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-02 14:44:51 +0100

    type: Determining the right solution for sign up form

---
+++ src/features/auth/types/sign-up.ts
@@ -0,0 +1,10 @@
+export interface  SignUpRequest {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    city: string;
+    dateOfBirth: Date;
+    gender: string;
+}
