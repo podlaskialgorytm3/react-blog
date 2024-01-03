0376581cd826ebb09be89fce35dfd98725474ff0
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-04 00:12:24 +0100

    feat: create dark theme and export that property to other components

---
+++ src/shared/themes/dark-theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      background: {
+        default: '#030712',
+      },
+    },
+  });
+
+export default darkTheme;
