b8e9edc3b393a2425365c3cd3a39cbed4f18feb4
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-04 16:17:27 +0100

    feat: use animation to main texts

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,23 @@
 import { MainDescription } from "../../features/home/main-description"
 import { MainTitle } from "../../features/home/main-title"
 
+import { useSpring, animated } from 'react-spring';
+
+
 export const Home = () => {
+
+
+    const props = useSpring({
+        opacity: 1,
+        from: { opacity: 0 },
+      });
+
     return (
        <main>
+        <animated.div style={props}>
             <MainTitle />
             <MainDescription />
+        </animated.div>
        </main>
     )
 }
