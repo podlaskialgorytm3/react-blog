e9b7dc36bc3e3fc0487552d65ca96b5726e8c409
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-07 16:10:23 +0100

    refactor: export object with styles to separated file

--- src/features/auth/constants/data.ts
+++ src/features/auth/constants/data.ts
@@ -9,3 +9,15 @@ export const DEFAULT_DATA: SignUpData = {
     city: '',
     dateOfBirth: '',
   };
+export const STYLES_BUTTON = {
+    mx: 'auto',
+    display: 'block',
+    mb: 2,
+    mt: 2,
+    color: 'white',
+    bgcolor: 'red',
+    '&:hover': {
+        bgcolor: 'red',
+        opacity: [0.9, 0.8, 0.7],
+    }
+}
