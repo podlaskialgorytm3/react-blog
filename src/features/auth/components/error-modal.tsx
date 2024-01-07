d9b2d7631d4a4480ebfd89170c92aed4a00a2853
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-08 00:46:05 +0100

    style: change theme of error pop-up

--- src/features/auth/components/error-modal.tsx
+++ src/features/auth/components/error-modal.tsx
@@ -11,13 +11,18 @@ export const ErrorModal:React.FC<ErrorModalData> = ({isOpen,closeModal,isError,e
                 isOpen={isOpen}
                 onRequestClose={closeModal}
                 ariaHideApp={false}
-                className="bg-white rounded-lg w-[350px] h-[350px] absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
+                style={{
+                    overlay: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    }
+                }}
+                className="bg-[#030712] rounded-lg w-[350px] h-[350px] absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]"
              >
                 {isError && (
                     <div className="flex flex-col">
                         <h1 className="text-[red] text-5xl text-center font-bold m-5">Error</h1>
                         <img src={errorIMG} alt="error" className="w-[100px] h-[100px] mx-auto"/>
-                        <p className="text-black p-3 text-center">{error.message}</p>
+                        <p className="text-white p-3 text-center">{error.message}</p>
                         <Button onClick={closeModal} sx={STYLES_BUTTON}>close</Button>
                     </div>
                 )}
