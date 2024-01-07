c3844f3cfe865871601aec6ecb4bbf443808990a
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-07 19:51:22 +0100

    feat: add error page to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { QueryClientProvider } from '@tanstack/react-query';
 
+import { ErrorPage } from "./pages/error/error-page"
 import { Root } from "./pages/root/root";
 import { SignIn } from "./pages/auth/sign-in"
 import { SignUp } from "./pages/auth/sign-up"
 import { Home } from "./pages/home/home"
+
 
 import { queryClient } from "./features/auth/utils/fetch-data"
 
@@ -12,6 +14,7 @@ const route = createBrowserRouter([
   {
     path: '/', 
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {path: '/', element: <Home />},
       {path: '/signin', element: <SignIn />},
---
+++ src/pages/error/error-page.tsx
@@ -0,0 +1,19 @@
+import { Menu } from "../../shared/components/menu"
+import { ThemeProvider } from '@mui/material/styles';
+import { Container } from "@mui/material";
+import {CssBaseline} from "@mui/material";
+import  darkTheme  from "../../shared/themes/dark-theme"
+
+export const ErrorPage = () => {
+    return(
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Menu />
+            <Container component="main" maxWidth="sm">
+            <div className="w-full h-[500px] flex justify-center items-center">
+                <h1 className="text-5xl font-bold text-white">404 Not Found</h1>
+            </div>
+            </Container>
+        </ThemeProvider>
+    )
+}
