bcfb52aaf44428fe8524a102ef56afa14a0ea7e3
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-15 18:36:09 +0100

    feat: read img data

--- src/features/user/components/profile.tsx
+++ src/features/user/components/profile.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "../../../shared/hooks/useAuth"
 
 
+
 const img = "https://img.freepik.com/premium-photo/chita_827316-164.jpg"
 const border = "border-[#41c48b] border-[3px]"
 
@@ -8,11 +9,15 @@ const border = "border-[#41c48b] border-[3px]"
 export const ProfileCard = () => {
     const { userData } = useAuth()
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.files)
+    }
+
     return(
         <>
             <div className={`w-[500px] h-[600px] flex flex-col items-center relative ${border}`}>
                 <img src={img} className={`w-[200px] h-[200px] mt-10 rounded-[50%] ${border}`}/>
-                <input type="file" accept="image/*" className="absolute top-20 left-40 w-[200px] h-[200px] block opacity-0"/>
+                <input type="file" accept="image/*" className="absolute top-20 left-40 w-[200px] h-[200px] block opacity-0" onChange={handleChange}/>
                 <h1 className="mt-10 text-5xl">{userData.first_name} {userData.last_name}</h1>
                 <p className="mt-10 text-3xl">{userData.city}</p>
                 <p className="mt-10 text-3xl">10 postów</p>
