2665111a7b57df9d516143405993b47f4d503356
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-15 14:46:00 +0100

    feat: create separated navigation menu for profile page

---
+++ src/features/user/components/navigation-menu.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+import AccountIcon from '../../../assets/account.png';
+import BlogIcon from '../../../assets/blog.png';
+import SettingsIcon from '../../../assets/settings.png';
+
+
+const imgStyle = "w-[70px] h-[70px] p-1 rounded-[10px]"
+
+export const NavigationMenu = () => {
+
+    return(
+        <div className={`mb-10 w-[500px] h-[100px] flex justify-around`}>
+                <NavLink to="/post-settings" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})} className={'h-[70px] rounded-3xl'}>
+                    <img src={BlogIcon} alt="Post Settings" className={imgStyle}/>
+                </NavLink>
+                <NavLink to="/profile" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})}  className={'h-[70px] rounded-3xl'}>
+                    <img src={AccountIcon} alt="Account Information" className={imgStyle}/>
+                </NavLink>
+                <NavLink to="/settings" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})}  className={'h-[70px] rounded-3xl'}>
+                    <img src={SettingsIcon} alt="Account Settings" className={imgStyle}/>
+                </NavLink>
+        </div>
+    )
+}
--- src/features/user/profile.tsx
+++ src/features/user/profile.tsx
@@ -1,14 +1,19 @@
 import { useAuth } from "../../shared/hooks/useAuth"
 
+import { NavigationMenu } from "./components/navigation-menu"
+
 const img = "https://img.freepik.com/premium-photo/chita_827316-164.jpg"
+const border = "border-[#41c48b] border-[3px]"
+
 
 export const ProfileCard = () => {
     const { userData } = useAuth()
 
     return(
         <>
-            <div className="w-[500px] h-[700px] border-[#41c48b] border-[3px] flex flex-col items-center relative">
-                <img src={img} className="w-[200px] h-[200px] mt-10 rounded-[50%] border-[#41c48b] border-[3px]"/>
+            <NavigationMenu/>
+            <div className={`w-[500px] h-[600px] flex flex-col items-center relative ${border}`}>
+                <img src={img} className={`w-[200px] h-[200px] mt-10 rounded-[50%] ${border}`}/>
                 <input type="file" accept="image/*" className="absolute top-20 left-40 w-[200px] h-[200px] block opacity-0"/>
                 <h1 className="mt-10 text-5xl">{userData.first_name} {userData.last_name}</h1>
                 <p className="mt-10 text-3xl">{userData.city}</p>
