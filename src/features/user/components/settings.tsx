3cb2e1445d5d58ef008689af81348ffe95d8b421
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-15 15:29:34 +0100

    feat: get data from from

--- src/features/user/components/settings.tsx
+++ src/features/user/components/settings.tsx
@@ -11,11 +11,25 @@ export const ProfileSettings = () => {
     const data = new Date(userData.date_of_birth);
     const date = `${data.getUTCFullYear()}-${String(data.getUTCMonth() + 1).padStart(2, '0')}-${String(data.getUTCDate()).padStart(2, '0')}`;
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData: any = new FormData(e.currentTarget);
+        const userFormData = {
+            first_name: formData.get('firstName'),
+            last_name: formData.get('lastName'),
+            city: formData.get('city'),
+            phone: formData.get('phone'),
+            date: formData.get('date'),
+        }
+        console.log(userFormData)
+    }
+
     return(
         <div>
             <h1 className="text-4xl">Profile Settings</h1>
             <Box
                 component="form"
+                onSubmit={handleSubmit}
                 sx={{
                     display: 'flex',
                     flexDirection: 'column',
@@ -25,16 +39,14 @@ export const ProfileSettings = () => {
                 noValidate
                 autoComplete="off"
             >
-                <TextField id="standard-basic" label="First Name" variant="standard" defaultValue={userData.first_name} />
-                <TextField id="standard-basic" label="Last Name" variant="standard" defaultValue={userData.last_name} />
-                <TextField id="standard-basic" label="City" variant="standard" defaultValue={userData.city}/>
-                <TextField id="standard-basic" label="Phone Number" variant="standard"  type="number" defaultValue={userData.phone} />
-                <TextField id="standard-basic" label="Date Of Birth" variant="standard" type='date' defaultValue={date}/>
+                <TextField id="standard-basic" label="First Name" variant="standard" defaultValue={userData.first_name} name="firstName"/>
+                <TextField id="standard-basic" label="Last Name" variant="standard" defaultValue={userData.last_name} name="lastName" />
+                <TextField id="standard-basic" label="City" variant="standard" defaultValue={userData.city} name="city"/>
+                <TextField id="standard-basic" label="Phone Number" variant="standard"  type="number" defaultValue={userData.phone} name="phone"/>
+                <TextField id="standard-basic" label="Date Of Birth" variant="standard" type='date' defaultValue={date} name="date"/>
                 <Button type="submit" variant="contained"
-                        sx={{ mt: 3, mb: 2 , bgcolor: '#41c48b', color: '#fff' ,'&:hover': {
-                                backgroundColor: '#328a63',
-                                opacity: [0.9, 0.8, 0.7],
-                            } }}>Confirm changes
+                        sx={{ mt: 3, mb: 2 , bgcolor: '#41c48b', color: '#fff' ,'&:hover': {backgroundColor: '#328a63',opacity: [0.9, 0.8, 0.7],}}}
+                >Confirm changes
                 </Button>
             </Box>
         </div>
