ecaa0c8af82b4e8cf47c805f4494006d7e2ef5b9
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-16 17:48:06 +0100

    feat: create function to update image

--- src/features/user/utils/fetch-data.ts
+++ src/features/user/utils/fetch-data.ts
@@ -25,3 +25,20 @@ export const updateUser = async (userData: ResultData) => {
 
     return data;
 };
+export const updateImage = async (formData: any,id: number) => {
+    const response = await fetch(`http://localhost:3000/update-image/${id}`,{
+        method: 'PUT',
+        mode: 'cors',
+        body: formData,
+        headers: {
+            'Content-Type': 'multipart/form-data', 
+        }
+    })
+    if(!response.ok){
+        const info = await response.json();
+        const error: any = new Error(info.details || 'Something went wrong');
+        throw error;
+    }
+    const { data } = await response.json();
+    return data;
+}
