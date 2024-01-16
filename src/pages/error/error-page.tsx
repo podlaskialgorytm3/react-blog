0388fed79050884f91444d904c7b02efa25e67a9
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-16 19:40:15 +0100

    perf: delete a few elements from Material UI

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,14 +1,10 @@
-import { Menu } from "../../shared/components/menu"
 import { ThemeProvider } from '@mui/material/styles';
 import { Container } from "@mui/material";
-import {CssBaseline} from "@mui/material";
 import  darkTheme  from "../../shared/themes/dark-theme"
 
 export const ErrorPage = () => {
     return(
         <ThemeProvider theme={darkTheme}>
-            <CssBaseline />
-            <Menu />
             <Container component="main" maxWidth="sm">
             <div className="w-full h-[500px] flex justify-center items-center">
                 <h1 className="text-5xl font-bold text-white">404 Not Found 👹</h1>
