5a6c2fe9792c3b13aeac8d9ca7d687c720d42401
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-18 20:07:53 +0100

    style: change style for navigation menu in user dashboard

--- src/features/user/components/navigation-menu.tsx
+++ src/features/user/components/navigation-menu.tsx
@@ -5,19 +5,20 @@ import BlogIcon from '../../../assets/blog.png';
 import SettingsIcon from '../../../assets/settings.png';
 
 
-const imgStyle = "w-[70px] h-[70px] p-1 rounded-[10px]"
+const imgStyle = "w-[50px] h-[50px] p-1 rounded-[10px]";
+const navlinkStyle = "h-[70px] w-[70px] rounded-[30px] flex justify-center items-center";
 
 export const NavigationMenu = () => {
 
     return(
         <div className={`mb-10 w-[500px] h-[100px] flex justify-around`}>
-                <NavLink to="/user/post-settings" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})} className={'h-[70px] rounded-3xl'}>
+                <NavLink to="/user/post-settings" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})} className={navlinkStyle}>
                     <img src={BlogIcon} alt="Post Settings" className={imgStyle}/>
                 </NavLink>
-                <NavLink to="/user/profile" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})}  className={'h-[70px] rounded-3xl'}>
+                <NavLink to="/user/profile" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})}  className={navlinkStyle}>
                     <img src={AccountIcon} alt="Account Information" className={imgStyle}/>
                 </NavLink>
-                <NavLink to="/user/settings" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})}  className={'h-[70px] rounded-3xl'}>
+                <NavLink to="/user/settings" style={({isActive}) => ({backgroundColor: isActive ? '#41c48b' : ''})}  className={navlinkStyle}>
                     <img src={SettingsIcon} alt="Account Settings" className={imgStyle}/>
                 </NavLink>
         </div>
