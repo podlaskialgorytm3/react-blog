5a1fd1908f9a28954c0cfdf1d70f25c2a6924844
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-18 20:40:54 +0100

    refactor: destroying the variable and saving the styles in the tailwind.

--- src/features/user/components/profile.tsx
+++ src/features/user/components/profile.tsx
@@ -4,8 +4,6 @@ import { updateImage } from "../api/update-image"
 import { useAuth } from "../../../shared/hooks/useAuth"
 
 const img = "https://img.freepik.com/premium-photo/chita_827316-164.jpg"
-const border = "border-[#41c48b] border-[3px]"
-
 
 export const ProfileCard = () => {
     const { userData } = useAuth()
@@ -27,8 +25,8 @@ export const ProfileCard = () => {
 
     return(
         <>
-            <div className={`w-[500px] h-[600px] flex flex-col items-center relative ${border}`}>
-                <img src={img} className={`w-[200px] h-[200px] mt-10 rounded-[50%] ${border}`}/>
+            <div className={`w-[500px] h-[600px] flex flex-col items-center relative border-[#41c48b] border-[3px]`}>
+                <img src={img} className={`w-[200px] h-[200px] mt-10 rounded-[50%] border-[#41c48b] border-[3px]`}/>
                 <input type="file" accept="image/*" className="absolute top-20 left-40 w-[200px] h-[200px] block opacity-0" onChange={handleChange}/>
                 <h1 className="mt-10 text-5xl">{userData.first_name} {userData.last_name}</h1>
                 <p className="mt-10 text-3xl">{userData.city}</p>
