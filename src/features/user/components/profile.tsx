ae4debc84e0fd016e016ac2eea4a6f6e94a63aea
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-19 21:00:54 +0100

    feat: create loading and handle errors

--- src/features/user/components/profile.tsx
+++ src/features/user/components/profile.tsx
@@ -7,6 +7,10 @@ import { ref,uploadBytes } from "firebase/storage"
 import { useQuery } from "@tanstack/react-query"
 import { fetchImage } from "../api/fetch-image";
 
+import { Loading } from "../../../shared/components/loading"
+
+import Swal from "sweetalert2"
+
 const img = "https://img.freepik.com/premium-photo/chita_827316-164.jpg"
 
 export const ProfileCard = () => {
@@ -27,8 +31,15 @@ export const ProfileCard = () => {
 
     return(
         <>
+            {isError && Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${error.message}`,
+                confirmButtonText: 'Okay',
+            })}
             <div className={`w-[500px] h-[600px] flex flex-col items-center relative border-[#41c48b] border-[3px]`}>
-                <img src={data ? data : img} className={`w-[200px] h-[200px] mt-10 rounded-[50%] border-[#41c48b] border-[3px]`}/>
+                <div className="mt-10">{isLoading && <Loading size={50}/> }</div>
+                {data && <img src={data ? data : img} alt="profile" className="w-[200px] h-[200px] rounded-full object-cover border-[#41c48b] border-[3px]"/>}
                 <input type="file" accept="image/*" className="absolute top-20 left-40 w-[200px] h-[200px] block opacity-0" onChange={handleChange}/>
                 <h1 className="mt-10 text-5xl">{userData.first_name} {userData.last_name}</h1>
                 <p className="mt-10 text-3xl">{userData.city}</p>
