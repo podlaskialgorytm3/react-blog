488e87f67d19d7edfb8c86f1ad690020df96c470
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 19:12:01 +0100

    fix: reapir styles in error page

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { Container } from "@mui/material";
 import  darkTheme  from "../../shared/themes/dark-theme"
+import { Menu } from "../../shared/components/menu"
+import { CssBaseline } from '@mui/material';
+const centering = {display: 'flex', flexDirection: 'column', alignItems: "center"}
 
 export const ErrorPage = () => {
     return(
         <ThemeProvider theme={darkTheme}>
-            <Container component="main" maxWidth="sm">
-            <div className="w-full h-[500px] flex justify-center items-center">
+            <CssBaseline />
+            <Menu />
+            <Container component="main" maxWidth="sm" sx={centering}>
                 <h1 className="text-5xl font-bold text-white">404 Not Found 👹</h1>
-            </div>
             </Container>
         </ThemeProvider>
     )
