93bd2e4e9a919038ce758364bccd6694877c9bb1
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 22:59:09 +0100

    feat: create post settings card in post setting page

---
+++ src/features/user/components/post-settings-card.tsx
@@ -0,0 +1,18 @@
+import { PostRequest } from "../../../shared/types/post-request"
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+export const PostSettingsCard = ({post}: {post: PostRequest}) => {
+    return(
+        <div className={`w-[500px] h-[100px] flex items-center justify-between flex-row m-5`}>
+            <div className="flex items-center">
+                <img src={post.image} alt={post.title} className="w-[100px] h-[100px] object-cover" />
+                <h1 className="text-[20px] ml-5">{post.title}</h1>
+            </div>
+            <div>
+                <EditIcon sx={{width: '45px', height: "45px"}}/>
+                <DeleteIcon sx={{width: '45px', height: "45px"}}/>
+            </div>
+        </div>
+    )
+}
