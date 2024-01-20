4fc1773c589d1fb40bd44ac1fb47257e33d73212
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 17:30:27 +0100

    feat: display posts in post container

---
+++ src/features/posts/components/post-card.tsx
@@ -0,0 +1,19 @@
+import { PostRequest } from "../types/post-content"
+
+const img = "https://img.freepik.com/premium-photo/chita_827316-164.jpg"
+const postImage = "https://uploads.sitepoint.com/wp-content/uploads/2017/04/1493235373large_react_apps_A-01.png"
+
+export const PostCard = ({ post }: { post: PostRequest }) => {
+    return (
+        <div className="w-[500px] h-[450px] m-5">
+            <div className="w-full h-[100px] flex flex-row items-center justify-start">
+                <img src={img} alt="post" className="w-[70px] m-5 object-cover rounded-[50%]" />
+                <p className="text-[24px]">Podlaski Alogrytm</p>
+            </div>
+            <div>
+                <img src={post.image ? post.image : postImage} alt="post" className="w-full h-[250px] object-cover rounded-[50px]" />
+                <h1 className="text-[24px] font-bold text-center mt-6">{post.title}</h1>
+            </div>
+        </div>
+    )
+}
--- src/features/posts/components/post-container.tsx
+++ src/features/posts/components/post-container.tsx
@@ -2,6 +2,8 @@ import { fetchPosts } from "../api/fetch-posts"
 import { PostRequest } from "../types/post-content"
 import { useQuery } from "@tanstack/react-query"
 
+import { PostCard } from "./post-card"
+
 export const PostContainer = () => {
     const { data } = useQuery({
         queryKey: ["posts"],
@@ -9,8 +11,8 @@ export const PostContainer = () => {
     })
     
     return (
-        <div className="w-[1200px] flex flex-wrap">
-            {data && data.map((post: PostRequest) => {console.log(post)})}
+        <div className="w-[1200px] flex flex-wrap justify-center">
+            {data && data.map((post: PostRequest) => <PostCard key={post.post_id} post={post} />)}
         </div>
     )
 }
