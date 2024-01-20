9836bf055bd7fee283c09c844c0df726b3bae75d
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 17:43:06 +0100

    feat: add loading and handling errors with sweetalert

--- src/features/posts/components/post-container.tsx
+++ src/features/posts/components/post-container.tsx
@@ -4,14 +4,28 @@ import { useQuery } from "@tanstack/react-query"
 
 import { PostCard } from "./post-card"
 
+import { Loading } from "../../../shared/components/loading"
+
+import Swal from "sweetalert2"
+
 export const PostContainer = () => {
-    const { data } = useQuery({
+    const { data, isLoading, isError, error } = useQuery({
         queryKey: ["posts"],
         queryFn: fetchPosts,
     })
+
+    if(isError){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.message,
+            confirmButtonText: "Ok"
+        })
+    }
     
     return (
         <div className="w-[1200px] flex flex-wrap justify-center">
+            {isLoading && <Loading  size={100}/>}
             {data && data.map((post: PostRequest) => <PostCard key={post.post_id} post={post} />)}
         </div>
     )
