75302ad1d99bde16b957dca5cbda7285c8a3a101
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 20:59:29 +0100

    feat: add loading bar and handling error by sweet alert

--- src/features/posts/components/post-page-container.tsx
+++ src/features/posts/components/post-page-container.tsx
@@ -3,17 +3,30 @@ import { fetchPost } from "../../../api/fetch-post"
 import { useQuery } from "@tanstack/react-query"
 import { PostPage } from "./post-page"
 
+import { Loading } from "../../../shared/components/loading"
+import Swal from "sweetalert2"
+
 export const PostPageContainer = () => {
     const { id } = useParams<{id: string}>();
 
-    const { data: post } = useQuery({
+    const { data: post, isLoading, isError, error } = useQuery({
         queryKey: ["post", id],
         queryFn: () => fetchPost(parseInt(id || ""))
     })
 
+    if(isError) {
+        Swal.fire({
+            title: "Error",
+            text: error?.message,
+            icon: "error",
+            confirmButtonText: "Ok"
+        })
+    }
+
     return (
         <>
-            {post && <PostPage post={post} />}
+            {isLoading && <Loading size={100} />}
+            {(!isLoading || post) && <PostPage post={post} />}
         </>
     )
 }
