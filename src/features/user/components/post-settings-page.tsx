fe4e5fc8b69a2f5b6b12cd73f9ee267c9fa9ec85
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 23:05:11 +0100

    feat: add loading bar and handling error with sweet alert

--- src/features/user/components/post-settings-page.tsx
+++ src/features/user/components/post-settings-page.tsx
@@ -3,18 +3,30 @@ import { fetchUserPost } from "../api/fetch-user-post"
 import { useAuth } from "../../../shared/hooks/useAuth"
 import { PostRequest } from "../../../shared/types/post-request"
 import { PostSettingsCard } from "./post-settings-card"
+import { Loading } from "../../../shared/components/loading"
+import Swal from "sweetalert2"
 
 export const PostSettingsPage = () => {
     const { userData } = useAuth();
 
-    const { data } = useQuery({
+    const { data, isLoading, isError, error } = useQuery({
         queryKey: ["post"],
         queryFn: () => fetchUserPost(userData.user_id)
     })
 
+    if(isError){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.message,
+            confirmButtonText: "Ok"
+        })
+    }
+
     return(
         <div className={`w-[500px] h-[auto] flex flex-col items-center relative`}>
-                <h1 className="text-[36px] mb-5">Post Settings 🔧🗂️</h1>   
+                <h1 className="text-[36px] mb-5">Post Settings 🔧🗂️</h1>
+                {isLoading && <Loading size={100} />}
                 {data && data.map((post: PostRequest) => (<PostSettingsCard key={post.post_id} post={post} />))}
          </div>
     )
