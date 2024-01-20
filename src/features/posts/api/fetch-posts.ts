f11b3007964e2376f8be08b25ceb5d955ae1eca4
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-20 18:19:28 +0100

    feat: return username post author to object with post

--- src/features/posts/api/fetch-posts.ts
+++ src/features/posts/api/fetch-posts.ts
@@ -1,4 +1,5 @@
 import { fetchImage } from "./fetch-image";
+import { fetchUsername } from "./fetch-username";
 
 export const fetchPosts = async () => {
     const response = await fetch('http://localhost:3000/fetch-posts');
@@ -9,12 +10,21 @@ export const fetchPosts = async () => {
 
     const posts = await response.json();
 
+    const usernamePromises = posts.map(async (post: any) => {
+        const userData = await fetchUsername(post.user_id);
+        post.username = userData; // Dodaj username do obiektu post
+    });
+
+    await Promise.all(usernamePromises);
+
     const imagePromises = posts.map(async (post: any) => {
         const image = await fetchImage(post.post_id);
         post.image = image;
     });
 
     await Promise.all(imagePromises);
+
+    console.log(posts)
 
     return posts;
 };
