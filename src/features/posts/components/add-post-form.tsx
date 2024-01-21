95586735c89a7ef3ce6a91bc68c5b271dc452376
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-21 14:15:40 +0100

    refactor: create a const with props for editor

--- src/features/posts/components/add-post-form.tsx
+++ src/features/posts/components/add-post-form.tsx
@@ -10,7 +10,7 @@ import { Button } from '@mui/material';
 import { ApiKeyTinyMMC } from '../../../shared/config/confidential-data';
 import { useCreatePost } from '../api/use-create-post';
 import { generateID } from '../utils/generate-id';
-
+import { EDITOR_INIT } from '../constants/editor-props';
 import { ref, uploadBytes } from "firebase/storage";
 import { imageDatabase } from '../../../shared/config/firebase-image';
 
@@ -98,17 +98,7 @@ export const AddPostForm = () => {
                 <Editor
                     apiKey={ApiKeyTinyMMC}
                     onChange={handleContentChange}
-                    init={{
-                        plugins: 'mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
-                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                        width: '800px',
-                        mergetags_list: [
-                            { value: 'First.Name', title: 'First Name' },
-                            { value: 'Email', title: 'Email' },
-                        ],
-                        skin: 'oxide-dark',
-                        language: 'en'
-                        }}
+                    init={EDITOR_INIT}
                     initialValue=""
                 />
                 <p className="text-red-500">{error.content}</p>
---
+++ src/features/posts/constants/editor-props.ts
@@ -0,0 +1,10 @@
+export const EDITOR_INIT = {
+    plugins: 'mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    width: '800px',
+    mergetags_list: [
+        { value: 'First.Name', title: 'First Name' },
+        { value: 'Email', title: 'Email' },
+    ],
+    skin: 'oxide-dark',
+    language: 'en'}
