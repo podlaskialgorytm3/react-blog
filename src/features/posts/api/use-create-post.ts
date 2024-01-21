9d0852e18780114b8064e2b688d9f8adc9084b93
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-21 13:44:01 +0100

    refactor: export function to createPost to other file with useCreatePost hook

--- src/features/posts/api/use-create-post.ts
+++ src/features/posts/api/use-create-post.ts
@@ -1,12 +1,32 @@
 import { useMutation } from '@tanstack/react-query';
-import { createPost } from '../api/create-post';
 import { queryClient } from '../../../api/query-client';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
+import { PostContent } from "../types/post-content"
+
+const createPost = async (postData: PostContent) => {
+    const response = await fetch('http://localhost:3000/create-post', {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(postData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if(!response.ok) {
+        const info = await response.json()
+        const error: object = new Error(info.details || 'Something went wrong')
+        throw error
+    }
+
+    const { data } = await response.json()
+
+    return data;
+}
+
 export const useCreatePost = () => {
     const navigate = useNavigate();
-
     const createPostMutation = useMutation({
         mutationFn: createPost,
         onSuccess: (data) => {
