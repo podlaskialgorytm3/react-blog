8f00ff5eae9be5619dbc3d0d3d06b82b70031aa6
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-22 20:29:17 +0100

    refactor: change function which returing useQuery and user photos

--- src/features/user/api/use-fetch-image.ts
+++ src/features/user/api/use-fetch-image.ts
@@ -1,13 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchProfileImage } from "./fetch-profile-image";
+import { fetchUserImage } from "../../../api/fetch-user-image";
 
-export const useFetchImage = (id: number) => {
-    
-   const fetchImageQuery = useQuery({
-        queryFn: () => fetchProfileImage(id),
+export const useFetchImage = (id: number) => (
+    useQuery({
+        queryFn: () => fetchUserImage(id),
         refetchOnWindowFocus: false,
         queryKey: ["uploads"],
-    });
-
-    return fetchImageQuery;
-}
+    })
+)
