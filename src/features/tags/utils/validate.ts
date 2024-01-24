4c2ea052832859123c6d596016c02d8e22208da9
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-24 22:21:21 +0100

    perf: improve validation of color

--- src/features/tags/utils/validate.ts
+++ src/features/tags/utils/validate.ts
@@ -1,6 +1,10 @@
 import { object, string  } from 'zod';
 
+const isRGBColor = (value: string) => /^rgb\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\)$/i.test(value);
+
 export const tagContentSchema = object({
     name: string().min(1, "Name can't be empty"),
-    color: string().min(1, "Color can't be empty"),
-})
+    color: string().refine(value => isRGBColor(value), {
+        message: "Invalid RGB color format"
+    })
+});
