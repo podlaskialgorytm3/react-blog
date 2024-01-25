b131303f85fd66babc90b1e88fa6a3f44bc8c29f
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-25 19:49:07 +0100

    feat: create type for tag response

--- src/features/posts/components/post-page.tsx
+++ src/features/posts/components/post-page.tsx
@@ -2,6 +2,7 @@ import { PostResponse } from "../../../shared/types/post-response"
 import { UserLabel } from "./user-label"
 import { TagLabel } from "../../../shared/components/tag";
 import { useFetchTagToPost } from "../../../api/use-fetch-tag-to-post";
+import { TagResponse } from "../../../shared/types/tag-response";
 
 export const PostPage = ({post}: {post: PostResponse}) => {
     const {data: tags} = useFetchTagToPost(post.post_id)
@@ -11,7 +12,7 @@ export const PostPage = ({post}: {post: PostResponse}) => {
             <h1 className="text-[36px]">{post.title}</h1>
             <div className="w-[1000px] overflow-hidden" dangerouslySetInnerHTML={{ __html: post.description }} />
             <div className="flex mt-20 flex-wrap">
-                {tags?.map((tag: any) => <TagLabel key={tag.tag_id} name={tag.name} color={tag.color} />)}
+                {tags?.map((tag: TagResponse) => <TagLabel key={tag.tag_id} name={tag.name} color={tag.color} />)}
             </div>
         </div>
     )
---
+++ src/shared/types/tag-response.ts
@@ -0,0 +1,5 @@
+export interface TagResponse {
+    tag_id: number;
+    name: string;
+    color: string;
+}
