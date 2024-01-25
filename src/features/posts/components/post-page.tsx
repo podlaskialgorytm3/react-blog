9d2c47b394b7f2fbcc114544dc1ed6271136cacc
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-25 18:08:40 +0100

    feat: view tags in page with post

--- src/features/posts/components/post-page.tsx
+++ src/features/posts/components/post-page.tsx
@@ -1,12 +1,18 @@
 import { PostResponse } from "../../../shared/types/post-response"
 import { UserLabel } from "./user-label"
+import { TagLabel } from "../../../shared/components/tag";
+import { useFetchTagToPost } from "../api/use-fetch-tag-to-post";
 
 export const PostPage = ({post}: {post: PostResponse}) => {
+    const {data: tags} = useFetchTagToPost(post.post_id)
     return (
         <div className="w-[1000px] flex flex-col items-center mt-10">
             <UserLabel user={post.user} />
             <h1 className="text-[36px]">{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.description }} />
+            <div className="w-[1000px] overflow-hidden" dangerouslySetInnerHTML={{ __html: post.description }} />
+            <div className="flex mt-20 flex-wrap">
+                {tags?.map((tag: any) => <TagLabel key={tag.tag_id} name={tag.name} color={tag.color} />)}
+            </div>
         </div>
     )
 }
