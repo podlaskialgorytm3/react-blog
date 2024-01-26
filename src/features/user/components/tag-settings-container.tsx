991d0b7d27fbedc5bf1500122b7db04f07b6c886
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-26 23:55:57 +0100

    feat: dispaly tags in tag settings

--- src/features/user/components/tag-settings-container.tsx
+++ src/features/user/components/tag-settings-container.tsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom"
+import { useAuth } from "../../../shared/hooks/useAuth"
+import { Loading } from "../../../shared/components/loading"
+import { useFetchTagUser } from "../api/use-fetch-tag-user"
+import { TagLabel } from "../../../shared/components/tag"
+import { TagResponse } from "../types/tag-response"
 
 export const TagSettingsContainer = () => {
+    const { userData } = useAuth();
+    const { data, isLoading, isError, error } = useFetchTagUser(userData.user_id)
+
     return(
         <div className={`w-[500px] h-[auto] flex flex-col items-center relative`}>
                 <h1 className="text-[36px] mb-5">Tag Settings 🔧🗂️</h1>
                 <p className="text-lg">Go to <Link to="/user/post-settings" className="text-main font-bold">Post Settings</Link></p>
+                {isLoading && <Loading size={100} />}
+                {!isLoading && data && data.map((tag: TagResponse) => <TagLabel key={tag.tag_id} color={tag.color} name={tag.name} />)}
          </div>
     )
 }
