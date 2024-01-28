36b606e8bbbc0fb8294a2458cdecc8a8a949fc01
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-28 22:21:26 +0100

    feat: add sanitization to view HTML code in post page

--- src/features/posts/components/post-page.tsx
+++ src/features/posts/components/post-page.tsx
@@ -4,13 +4,19 @@ import { TagLabel } from "../../../shared/components/tag";
 import { useFetchTagToPost } from "../../../api/use-fetch-tag-to-post";
 import { TagResponse } from "../../../shared/types/tag-response";
 
+import DOMPurify from 'dompurify';
+
+
 export const PostPage = ({post}: {post: PostResponse}) => {
     const {data: tags} = useFetchTagToPost(post.post_id)
+    
+    const sanitizedHTML = DOMPurify.sanitize(post.description);
+
     return (
         <div className="md:w-[1000px] flex flex-col items-center mt-10">
             <UserLabel user={post.user} />
             <h1 className="text-[36px]">{post.title}</h1>
-            <div className="md:w-[1000px] w-[350px] overflow-hidden" dangerouslySetInnerHTML={{ __html: post.description }} />
+            <div className="md:w-[1000px] w-[350px] overflow-hidden" dangerouslySetInnerHTML={{ __html: sanitizedHTML}}/>
             <div className="flex mt-20 flex-wrap">
                 {tags?.map((tag: TagResponse) => <TagLabel key={tag.tag_id} name={tag.name} color={tag.color} />)}
             </div>
