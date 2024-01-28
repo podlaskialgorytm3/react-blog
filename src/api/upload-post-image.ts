25b4b2a9b35329b3da367cc28d036dbe266af595
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-28 21:57:12 +0100

    refactor: change type of image to Blob type

--- src/api/upload-post-image.ts
+++ src/api/upload-post-image.ts
@@ -1,7 +1,7 @@
 import { ref, uploadBytes } from "firebase/storage";
 import { imageDatabase } from '../shared/config/firebase-image';
 
-export const uploadImage = (image: any,id: number) => {
+export const uploadImage = (image: Blob,id: number) => {
     uploadBytes(ref(imageDatabase, `posts/${id}`), image).then(() => {
         window.location.reload();
     })
