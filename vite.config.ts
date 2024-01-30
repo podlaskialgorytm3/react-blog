82ea0293a27fbce5e3993b2ea4a9dcc0f9034d67
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-30 23:20:49 +0100

    fix: resolve problem with .env and deploy env to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const cherryPickedKeys = [
+  "REACT_APP_API_URL",
+  "REACT_APP_API_TINY",
+  "REACT_APP_APP_ID",
+  "REACT_APP_MESSAGING_SENDER_ID",
+  "REACT_APP_STORAGE_BUCKET",
+  "REACT_APP_PROJECT_ID",
+  "REACT_APP_AUTH_DOMAIN",
+  "REACT_APP_API_KEY"
+];
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
+
+  return {
+    define: {
+      'process.env': processEnv
+    },
+    plugins: [react()],
+  }
 })
