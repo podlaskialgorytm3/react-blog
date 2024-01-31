f7c431b98140703b71e7fdc7e2daee1bbea6a9b7
podlaskialgorytm3 <live.m851@gmail.com>
2024-01-31 20:59:25 +0100

    feat: improve error page with menu component

--- src/pages/error/error-page.tsx
+++ src/pages/error/error-page.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { Container } from "@mui/material";
 import  darkTheme  from "../../shared/themes/dark-theme"
 import { CssBaseline } from '@mui/material';
+import { Menu } from "../../shared/components/Menu";
 
 const centering = {display: 'flex', flexDirection: 'column', alignItems: "center"}
 
@@ -9,6 +10,7 @@ export const ErrorPage = () => {
     return(
         <ThemeProvider theme={darkTheme}>
             <CssBaseline />
+            <Menu />
             <Container component="main" maxWidth="sm" sx={centering}>
                 <h1 className="text-5xl font-bold text-white">404 Not Found 👹</h1>
             </Container>
